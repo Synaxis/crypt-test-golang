e5c747f6829ffbde78025152e1a446eba1dc74c3
oiacow <oiacow@gmail.com>
2019-01-29 00:11:40 -0200

    added import packages

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io"
+	//"io/ioutil"
+	//"os"
+)
 func crateHash(key strng) strng {
 	hasher := md5.New()
-	hasher.Write([]byte(key)
+	hasher.Write([]byte(key))
 	return hex.EncodeToString(hasher.Sum(nil))
 }
 
@@ -11,9 +22,11 @@ func encrypt(data []byte, passphrase string) []byte {
 	gcm, _ := cipher.NewGCM(block)
 	nonce := make([]byte, gcm.NonceSize())
 	io.ReadFull(rand.Reader, nonce) //random is good for encryption guess why
-	ciphertext := gcm.Seal(nonce)
+	ciphertext := gcm.Seal(nonce, nonce, nil)
+	return ciphertext
 } 
 
 func main() {
-
+	ciphertext := encrypt([]byte("Hello Wolrd this will be encrypted"), "password")
+	fmt.Println(ciphertext)
 }
